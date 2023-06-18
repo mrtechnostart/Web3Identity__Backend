36287c6cca2e0859480964edb29071ab6f542e37
mrtechnostart <rambpandey238@gmail.com>
2023-06-18 22:57:18 +0530

    Done with manage frontend scripts

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -1,5 +1,10 @@
 const developmentChains = ["hardhat", "localhost"]
 
+
+const frontEndContractsFile = "/home/mrtechnostart/Desktop/Identity__Frontend/identity/src/Constants/contractAddress.json"
+const frontEndAbiFile = "/home/mrtechnostart/Desktop/Identity__Frontend/identity/src/Constants/abi.json"
 module.exports = {
-    developmentChains
+    developmentChains,
+    frontEndContractsFile,
+    frontEndAbiFile
 }
